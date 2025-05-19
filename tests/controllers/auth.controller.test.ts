98742cc6e72360fbe89d7b68e8a0b460cc4b3249
James Michael <jomichael1994@gmail.com>
2025-05-19 22:53:33 +0100

    update auth controller tests

--- tests/controllers/auth.controller.test.ts
+++ tests/controllers/auth.controller.test.ts
@@ -1,13 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import authController from '../../src/controllers/auth.controller';
-import User from '../../src/db/models/User';
 import * as authModel from '../../src/models/auth.model';
 
 jest.mock('../../src/models/auth.model');
 jest.mock('../../src/db/models/User');
 
 describe('auth.controller', () => {
-	let req: Partial<Request>;
+	let req: Partial<Request> = { body: {} };
 	let res: Partial<Response>;
 	let next: Partial<NextFunction>;
 	let statusMock: jest.Mock;
@@ -17,7 +16,6 @@ describe('auth.controller', () => {
 		jest.clearAllMocks();
 		statusMock = jest.fn().mockReturnThis();
 		jsonMock = jest.fn();
-		req = { body: {} };
 		res = {
 			status: statusMock,
 			json: jsonMock,
@@ -28,18 +26,14 @@ describe('auth.controller', () => {
 	describe('Register', () => {
 		it('Should create the user and return a token', async () => {
 			(authModel.userExists as jest.Mock).mockResolvedValue(false);
-			(authModel.generateHashedPassword as jest.Mock).mockResolvedValue(
-				'test-hashed-password'
-			);
-			(authModel.getToken as jest.Mock).mockReturnValue('test-jwt-token');
-
-			const saveMock = jest.fn().mockResolvedValue(undefined);
-			(User as jest.Mock).mockImplementation(() => ({
-				save: saveMock,
-				id: '123456',
-				email: 'test@example.com',
-				name: 'Test User',
-			}));
+			(authModel.registerUser as jest.Mock).mockResolvedValue({
+				token: 'test-token',
+				user: {
+					id: 'test-user-id',
+					email: 'john.doe@example.com',
+					name: 'John Doe',
+				},
+			});
 
 			req.body = {
 				email: 'test@example.com',
@@ -53,14 +47,13 @@ describe('auth.controller', () => {
 				next as NextFunction
 			);
 
-			expect(saveMock).toHaveBeenCalled();
 			expect(statusMock).toHaveBeenCalledWith(200);
 			expect(jsonMock).toHaveBeenCalledWith({
-				token: 'test-jwt-token',
+				token: 'test-token',
 				user: {
-					id: '123456',
-					name: 'Test User',
-					email: 'test@example.com',
+					id: 'test-user-id',
+					name: 'John Doe',
+					email: 'john.doe@example.com',
 				},
 			});
 		});
@@ -87,17 +80,9 @@ describe('auth.controller', () => {
 
 		it('Should respond with a 500 if an error is thrown', async () => {
 			(authModel.userExists as jest.Mock).mockResolvedValue(false);
-			(authModel.generateHashedPassword as jest.Mock).mockRejectedValue(
+			(authModel.registerUser as jest.Mock).mockRejectedValue(
 				new Error('Nope, cannot hash password.')
 			);
-
-			const saveMock = jest.fn().mockResolvedValue(undefined);
-			(User as jest.Mock).mockImplementation(() => ({
-				save: saveMock,
-				id: '123456',
-				email: 'test@example.com',
-				name: 'Test User',
-			}));
 
 			req.body = {
 				email: 'test@example.com',
@@ -111,107 +96,81 @@ describe('auth.controller', () => {
 				next as NextFunction
 			);
 
-			expect(saveMock).not.toHaveBeenCalled();
 			expect(statusMock).toHaveBeenCalledWith(500);
 			expect(jsonMock).toHaveBeenCalledWith({
 				message: 'Something went wrong.',
 			});
 		});
+	});
 
-		describe('Login', () => {
-			it('Should successfully log in a user and return a token', async () => {
-				const user = {
-					id: '123456',
-					email: 'test@example.com',
-					name: 'Test User',
-				};
-				(authModel.logInUser as jest.Mock).mockResolvedValue(user);
-				(authModel.getToken as jest.Mock).mockReturnValue(
-					'test-jwt-token'
-				);
-
-				req.body = { email: 'test@example.com', password: 'password' };
-
-				await authController.login(
-					req as Request,
-					res as Response,
-					next as NextFunction
-				);
-
-				expect(statusMock).toHaveBeenCalledWith(200);
-				expect(jsonMock).toHaveBeenCalledWith({
-					token: 'test-jwt-token',
-					user,
-				});
+	describe('Login', () => {
+		it('Should successfully log in a user and return a token', async () => {
+			const user = {
+				id: '123456',
+				email: 'test@example.com',
+				name: 'Test User',
+			};
+			(authModel.logInUser as jest.Mock).mockResolvedValue({
+				token: 'test-token',
+				user,
 			});
 
-			it('Should respond with a 401 when invalid credentials are provided', async () => {
-				(authModel.logInUser as jest.Mock).mockRejectedValue(
-					new Error('Invalid credentials.')
-				);
+			req.body = { email: 'test@example.com', password: 'password' };
 
-				req.body = {
-					email: 'test@example.com',
-					password: 'definitely-not-the-correct-password',
-				};
+			await authController.login(
+				req as Request,
+				res as Response,
+				next as NextFunction
+			);
 
-				await authController.login(
-					req as Request,
-					res as Response,
-					next as NextFunction
-				);
-
-				expect(statusMock).toHaveBeenCalledWith(401);
-				expect(jsonMock).toHaveBeenCalledWith({
-					message: 'Invalid credentials.',
-				});
+			expect(statusMock).toHaveBeenCalledWith(200);
+			expect(jsonMock).toHaveBeenCalledWith({
+				token: 'test-token',
+				user,
 			});
+		});
 
-			it('Should respond with a 500 if an unknown error is thrown by logInUser', async () => {
-				(authModel.logInUser as jest.Mock).mockRejectedValue(
-					new Error('This error is a mystery.')
-				);
+		it('Should respond with a 401 when invalid credentials are provided', async () => {
+			(authModel.logInUser as jest.Mock).mockRejectedValue(
+				new Error('Invalid credentials.')
+			);
 
-				req.body = {
-					email: 'test@example.com',
-					password: 'password',
-				};
+			req.body = {
+				email: 'test@example.com',
+				password: 'definitely-not-the-correct-password',
+			};
 
-				await authController.login(
-					req as Request,
-					res as Response,
-					next as NextFunction
-				);
+			await authController.login(
+				req as Request,
+				res as Response,
+				next as NextFunction
+			);
 
-				expect(statusMock).toHaveBeenCalledWith(500);
-				expect(jsonMock).toHaveBeenCalledWith({
-					message: 'Something went wrong.',
-				});
+			expect(statusMock).toHaveBeenCalledWith(401);
+			expect(jsonMock).toHaveBeenCalledWith({
+				message: 'Invalid credentials.',
 			});
+		});
 
-			it('Should respond with a 500 if an unknown error is thrown by getToken', async () => {
-				const user = {
-					id: '123456',
-					email: 'test@example.com',
-					name: 'Test User',
-				};
-				(authModel.logInUser as jest.Mock).mockResolvedValue(user);
-				(authModel.getToken as jest.Mock).mockImplementation(() => {
-					throw new Error('Another mystery error.');
-				});
+		it('Should respond with a 500 if an unknown error is thrown by logInUser', async () => {
+			(authModel.logInUser as jest.Mock).mockRejectedValue(
+				new Error('This is a mystery error...')
+			);
 
-				req.body = { email: 'test@example.com', password: 'password' };
+			req.body = {
+				email: 'test@example.com',
+				password: 'password',
+			};
 
-				await authController.login(
-					req as Request,
-					res as Response,
-					next as NextFunction
-				);
+			await authController.login(
+				req as Request,
+				res as Response,
+				next as NextFunction
+			);
 
-				expect(statusMock).toHaveBeenCalledWith(500);
-				expect(jsonMock).toHaveBeenCalledWith({
-					message: 'Something went wrong.',
-				});
+			expect(statusMock).toHaveBeenCalledWith(500);
+			expect(jsonMock).toHaveBeenCalledWith({
+				message: 'Something went wrong.',
 			});
 		});
 	});
